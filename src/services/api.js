e8c5bc2f4b3e29b1891efd21030e173aadde10dc
Carlos Rodrigo <rodrigolimac137@gmail.com>
2022-02-09 20:22:16 -0300

    teste de Merge

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 async function registerClient(body) {
     const promise = axios.post(`${BASE_URL}/register-client`, body);
-
     return promise;
 }
 
