4a9ee2370bec21ec7ff842089d83eb11c80e18bb
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-11 15:46:16 -0300

    production

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 function createConfig(token) {
   return {
