75ddd72bbba97c8d9aeef417b4cc22c0256f6fab
Carlos Rodrigo <rodrigolimac137@gmail.com>
2022-02-10 21:07:41 -0300

    Feat: Main Acess Products

--- src/pages/client/Main/Main.js
+++ src/pages/client/Main/Main.js
@@ -1,11 +1,133 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import api from '../../../services/api';
 import styled from "styled-components";
+import CartAdd from "../../../assets/Cart_Add.png";
 
 export default function Main() {
-  return <Container></Container>;
+  const [allProducts, setAllProducts] = useState([])
+
+  function loadProducts(){
+    const promise= api.getAllProducts();
+
+    promise.then((response)=>{
+      setAllProducts(response.data);
+    });
+    promise.catch((error)=>{
+      console.log(error);
+    })
+  }
+
+  useEffect( loadProducts, []);
+
+  if (allProducts.length ===0){
+    return (
+    <Container>
+      <ProductBlock>
+        <CartButton onClick={()=>alert("adicionar no carrinho")}>
+          <img src={CartAdd} alt="Cart-Add" />
+        </CartButton>
+        <img />
+        <ProductText>
+          <h3>Celular</h3>
+          <h2>Celular bom</h2>
+          <h1>R$ 15.000,00</h1>
+        </ProductText>
+      </ProductBlock>
+      <ProductBlock>
+        <CartButton onClick={()=>alert("adicionar no carrinho")}>
+          <img src={CartAdd} alt="Cart-Add" />
+        </CartButton>
+        <img />
+        <ProductText>
+          <h3>Outro Celular</h3>
+          <h2>Celular ruim</h2>
+          <h1>R$ 30.000,00</h1>
+        </ProductText>
+      </ProductBlock>
+    </Container>
+    );
+  }
+  return (
+  <Container>
+    {allProducts.map((product)=>{
+      <ProductBlock key={product._key}>
+        <CartButton onClick={()=>alert("adicionar no carrinho")}>
+          <img src={CartAdd} alt="Cart-Add" />
+        </CartButton>
+        <img src={product.img} alt={product.name}/>
+        <ProductText>
+          <h3>{product.name}</h3>
+          <h2>{product.description}</h2>
+          <h1>{product.price}</h1>
+        </ProductText>
+      </ProductBlock>
+    })}
+  </Container>
+  );
 }
 
 const Container = styled.div`
   width: 80vw;
   height: 100vh;
+  padding-top: 15vh;
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+
+  box-sizing: border-box;
+
+  background-color: #FAFAFA;
+`;
+
+
+
+const ProductBlock = styled.div`
+  width: 224px;
+  height: 330px;
+
+  position: relative;
+  box-sizing: border-box;
+
+  border-radius: 5px;
+  border: 1px solid #DBDBDB;
+
+  img{
+    width: 224px;
+    height: 224px;
+  }
+`;
+
+const ProductText = styled.div`
+  padding: 20px 16px;
+
+  h1{
+    color: #00a650;
+    
+    font-size: 24px;
+  }
+  h2{
+    color: #333;
+    font-size: 14px;
+  }
+  h3{
+    color: #333;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 16px;
+  }
+`;
+
+const CartButton = styled.div`
+  right: 10px;
+  top: 10px;
+
+  position: absolute;
+
+  img{
+    width: 36px;
+    height: 36px;
+  }
 `;
--- src/services/api.js
+++ src/services/api.js
@@ -2,20 +2,43 @@ import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
+function createConfig(token) {
+    return {
+      headers: {
+        'Authorization': token
+      }
+    }
+  }
+
 async function registerClient(body) {
-    const promise = axios.post(`${BASE_URL}/register-client`, body);
+    const promise = axios.post(`${BASE_URL}/client/register`, body);
     return promise;
 }
 
 async function loginClient(body) {
-    const promise = axios.post(`${BASE_URL}/login-client`, body);
+    const promise = axios.post(`${BASE_URL}/client/login`, body);
+
+    return promise;
+}
+
+async function getAllProducts() {
+    const promise = axios.get(`${BASE_URL}/products`);
+
+    return promise;
+}
+
+async function getClientContent(token) {
+    const config = createConfig(token);
+    const promise = axios.get(`${BASE_URL}/client/get-user`, config)
 
     return promise;
 }
 
 const api = {
     registerClient,
-    loginClient
+    loginClient,
+    getAllProducts,
+    getClientContent
 }
 
 export default api;
