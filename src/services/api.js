33f9a67410e843ffd87dd8baee6df444848b5285
barbosaoli <gustavobarbosa7162@gmail.com>
2022-02-13 14:59:45 -0300

    fix bugs

--- src/services/api.js
+++ src/services/api.js
@@ -67,7 +67,7 @@ async function postSendToCart(token, id) {
 
 async function postConfirmBuy(token) {
   const config = createConfig(token);
-  const promise = axios.post(`${BASE_URL}/confirm-buy`, config);
+  const promise = axios.post(`${BASE_URL}/confirm-buy`, {}, config);
 
   return promise;
 }
